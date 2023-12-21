03491097383ce59745b3f0e7d87ffb6c041a331e
oliveur83 <tomolivierpro@gmail.com>
2023-12-21 09:14:34 +0100

    back exel

--- backend/src/controllers/dijkstra.ts
+++ backend/src/controllers/dijkstra.ts
@@ -2,9 +2,13 @@ import express from 'express';
 
 export const dijkstra = (req: express.Request, res: express.Response) => {
     try {
-        console.log('tom est un gros connard');
+        chemin_de_dijkstra()
     } catch (error) {
         console.log(error);
         res.sendStatus(404);
     }
+}
+function chemin_de_dijkstra()
+{
+    
 }
