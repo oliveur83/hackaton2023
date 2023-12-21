5d13271ee32efa741e8ae75a0a4a8b3bcb1af75a
oliveur83 <tomolivierpro@gmail.com>
2023-12-21 20:29:34 +0100

    back fini

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,15 +1,15 @@
 import express from 'express';
 // Assurez-vous que le chemin d'importation est correct et utilisez des accolades si nécessaire
-// Assurez-vous que le chemin d'importation est correct et utilisez des accolades si nécessaire
+ // Assurez-vous que le chemin d'importation est correct et utilisez des accolades si nécessaire
+ // Assurez-vous que le chemin d'importation est correct et utilisez des accolades si nécessaire
 import { all_ville } from '../controllers/dijkstra';
 import { ville_gps } from '../controllers/dijkstra'; 
-
 const router = express.Router();
 
 // Définition et exportation de la fonction qui renvoie le routeur
 export default () => {
 
     router.get('/all_ville', all_ville);
-    router.post('/dijkstra', ville_gps);
-   return router;
+    router.get('/dijkstra', ville_gps);
+    return router;
 };
